2224f5fda1a24050a0da87082b9255506d9619fd
Artur <asr.engsoft@gmail.com>
2025-12-18 14:16:01 -0300

    Remove hardcoded tutor addition from Main.java
    Eliminated the direct addition of a sample Tutor object in Main.java to avoid hardcoded test data in production code. Also removed template comments from ListaGerenciarTutores.java for code cleanliness.

--- src/clinicaveterinaria/Main.java
+++ src/clinicaveterinaria/Main.java
@@ -1,7 +1,5 @@
 package clinicaveterinaria;
 
-import clinicaveterinaria.controller.TutorController;
-import clinicaveterinaria.model.Tutor;
 import clinicaveterinaria.view.Menu;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -13,14 +11,6 @@ public class Main {
         } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
         
         }
-
-        TutorController.listaTutores.add(new Tutor(
-            "Artur Saraiva Rabelo", 
-            "Artur@gmail.com", 
-            "(99) 98166-4707", 
-            "Morada Nova - CE", 
-            "089.570.333-51"
-        ));
         
         Teste.testeTutores();
         Teste.testeVeterinarios();
